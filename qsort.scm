9636a576e94cd7ec892fc2218b6af114ba63b801
sniperHW <huangweilook@21cn.com>
2014-09-21 21:28:09 +0800

    none

--- qsort.scm
+++ qsort.scm
@@ -1,18 +1,25 @@
-(define my-filter
-    (lambda (f l)
-        (define cps
-            (lambda (l k)
-                (if (not (pair? l)) (k '())
-                    (let ([h (car l)])
-                        (if (f h) (cps (cdr l) (lambda (x) (k (cons h x))))
-                            (cps (cdr l) (lambda (x) (k x))))))))
-    (cps l (lambda (x) x))))
-(define qsort
-    (lambda (l)
-        (if (not (pair? l)) '()
-            (let* ([m (car l)]
-                   [large (my-filter (lambda (x) (if (> x m) #t #f)) (cdr l))]
-                   [less (my-filter (lambda (x) (if (<= x m) #t #f)) (cdr l))])
-            (append (qsort less) (cons m (qsort large)))))))
+	(define (foldl f init xs)
+		(define (iter xs acc)
+			(if (null? xs) acc
+				(iter (cdr xs) (f acc (car xs)))))
+		(iter xs init))
+	
+	(define (foldr f init xs)
+		(define (iter xs acc)
+			(if (null? xs) acc
+				(iter (cdr xs) (f (car xs) acc))))
+		(iter (reverse xs) init))
+	
+	(define (qsort l greater)
+		(if (not (pair? l)) '()
+			(let ([m (car l)]
+			     [partition (foldr (lambda (x acc)
+					         (let ([small (car acc)]
+						       [large (cadr acc)])
+						 (if (greater x m) (list small (cons x large))
+						     (list (cons x small) large))))
+					'(()()) (cdr l))])  
+			(append (qsort (car partition) greater) 
+			        (cons m (qsort (cadr partition) greater))))))
 				  
 				    	
